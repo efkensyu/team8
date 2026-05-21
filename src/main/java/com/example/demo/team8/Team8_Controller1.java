e043059723b1ee8d0a0c9424d266757b54584f2a
ikebe <n_ikebe@bold.ne.jp>
2026-05-21 15:50:16 +0900

    スタート画面から質問画面作成

--- src/main/java/com/example/demo/team8/Team8_Controller1.java
+++ src/main/java/com/example/demo/team8/Team8_Controller1.java
@@ -1,33 +1,37 @@
 package com.example.demo.team8;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@Controller
 public class Team8_Controller1 {
 	@GetMapping("/team8_1")
 	public String index() {
+		System.out.println("getメソッド実行");
 		return "team8/team8_start";
 	}
 	
 	
 	@PostMapping(value = "/team8_1", params = "next")
-	public String send1() {
-		return "team8/team8_question1";
+	public void send1() {
+//		return "team8/team8_question1";
 	}
 	
-	@PostMapping(value = "/team8_1", params = {"answer1","next1"})
+	/*
+	@PostMapping(value = "/team8_2", params = {"answer1","next1"})
 	public String send2() {
 		return "team8/team8_question2-1";
 	}
 	
-	@PostMapping(value = "/team8_1", params = {"answer2","next1"})
+	@PostMapping(value = "/team8_2", params = {"answer2","next1"})
 	public String send3() {
 		return "team8/team8_question2-2";
 	}
 	
-	@PostMapping(value = "/team8_1", params = "back1")
+	@PostMapping(value = "/team8_2", params = "back1")
 	public String send5() {
 		return "team8/team8_starts";
 	}
-	
+	*/
 }
