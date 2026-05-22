05ac30d80cec3505cb6239feabf029af45fbc411
mochizuki <r_mochizuki@bold.ne.jp>
2026-05-22 12:30:04 +0900

    3-1と2-2にValidation追加

--- src/main/java/com/example/demo/team8/Team8_Controller3.java
+++ src/main/java/com/example/demo/team8/Team8_Controller3.java
@@ -1,36 +1,41 @@
 package com.example.demo.team8;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class Team8_Controller3 {
 	@GetMapping("/team8_2-2")
-	public String index() {
+	public String index(@ModelAttribute("empForm") Team8_EmpForm2_2 empForm) {
 		System.out.println("indexメソッド実行");
 		return "team8/team8_question2-2";
 	}
 	
 	@PostMapping(value = "/team8_2-2", params = "next")
-	public String send1(@RequestParam ("question2") String question2) {
-		//↓これはうまく分岐できないときに使ってね
-		//System.out.println(question2);
+	public String send1(@ModelAttribute("empForm") @Validated Team8_EmpForm2_2 empForm, BindingResult result) {
 		
-		if (question2.equals("answer1")) {
+		if (result.hasErrors()) {
+			return "team8/team8_question2-2";
+		}
+		
+		if (empForm.getQuestion2_2().equals("answer1")) {
+			System.out.println(empForm.getQuestion2_2());
 			return "team8/team8_question3-3";
-		} else if (question2.equals("answer2")) {
+		} else if (empForm.getQuestion2_2().equals("answer2")) {
 			return "team8/team8_question3-4";
 		}
 		
-		return "team8/team8_question3-3";
+		return "team8/team8_kekka";
 	}
 	
 	
 	@PostMapping(value = "/team8_2-2", params = "back")
 	public String send3() {
-		//System.out.println("send3実行");
+		System.out.println("send3実行");
 		return "team8/team8_question1";
 	}
 }
--- src/main/java/com/example/demo/team8/Team8_Controller4.java
+++ src/main/java/com/example/demo/team8/Team8_Controller4.java
@@ -1,26 +1,32 @@
 package com.example.demo.team8;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class Team8_Controller4 {
 	@GetMapping("/team8_3-1")
-	public String index() {
+	public String index(@ModelAttribute("empForm") Team8_EmpForm3_1 empForm) {
 		System.out.println("indexメソッド実行");
 		return "team8/team8_question3-1";
 	}
 	
 	@PostMapping(value = "/team8_3-1", params = "next")
-	public String send1(@RequestParam ("question3") String question3) {
-		//↓これはうまく分岐できないときに使ってね
-		System.out.println(question3);
+	public String send1(@ModelAttribute("empForm") @Validated Team8_EmpForm3_1 empForm, BindingResult result) {
 		
-		if (question3.equals("answer1")) {
+		if (result.hasErrors()) {
+			return "team8/team8_question3-1";
+		}
+		
+		if (empForm.getQuestion3_1().equals("answer1")) {
+			System.out.println(empForm.getQuestion3_1());
 			return "team8/team8_kekka";
-		} else if (question3.equals("answer2")) {
+		} else if (empForm.getQuestion3_1().equals("answer2")) {
+			System.out.println(empForm.getQuestion3_1());
 			return "team8/team8_kekka";
 		}
 		
