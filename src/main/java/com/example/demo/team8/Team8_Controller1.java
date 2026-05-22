46637124c227c326545fb2cf7b69352436ba8a0a
ikebe <n_ikebe@bold.ne.jp>
2026-05-22 12:45:03 +0900

    質問画面１のvalidation追加途中

--- src/main/java/com/example/demo/team8/Team8_Controller1.java
+++ src/main/java/com/example/demo/team8/Team8_Controller1.java
@@ -3,6 +3,7 @@ package com.example.demo.team8;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class Team8_Controller1 {
@@ -14,25 +15,43 @@ public class Team8_Controller1 {
 	
 	
 	@PostMapping(value = "/team8_1", params = "next")
-	public void send1() {
-//		return "team8/team8_question1";
+	public String send1() {
+		System.out.println("send1メソッド実行");
+		return "team8/team8_question1";
 	}
 	
-	/*
-	@PostMapping(value = "/team8_2", params = {"answer1","next1"})
-	public String send2() {
-		return "team8/team8_question2-1";
-	}
 	
-	@PostMapping(value = "/team8_2", params = {"answer2","next1"})
-	public String send3() {
+	@PostMapping(value = "/team8_1", params = "next1")
+	public String send2(@RequestParam String question1) {
+		System.out.println("send2メソッド実行");
+		if(question1.equals("answer1")) {
+			return "team8/team8_question2-1";
+		}
+		
 		return "team8/team8_question2-2";
+		
 	}
 	
-	@PostMapping(value = "/team8_2", params = "back1")
-	public String send5() {
-		return "team8/team8_starts";
+	@PostMapping(value = "/team8_1", params = "next1")
+	public String send3(@RequestParam String question2_1) {
+		if(question2_1.equals("answer1")) {
+			return "team8/team8_question3-1";
+		}
+		return "team8/team8_question3-2";
 	}
-	*/
-	//あいうえお
+	
+	@PostMapping(value = "/team8_1", params = "next")
+	public String send4(@RequestParam String question2_2) {
+		if(question2_2.equals("answer1")) {
+			return "team8/team8_question3-3";
+		}
+		return "team8/team8_question3-4";
+	}
+	
+	@PostMapping(value = "/team8_1", params = "back1")
+	public String send5() {
+
+		return "team8/team8_start";
+	}
+	
 }
